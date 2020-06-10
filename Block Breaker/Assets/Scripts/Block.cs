ea3f343849f9450ab464c701f15a6c4c449461db
unknown <evie@davisplanet.com>
2020-06-10 12:50:14 -0400

    done with 1/2 course

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
+
+    // cached ref
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
-        Debug.Log(collision.gameObject.name);
+        
     }
 
 }
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] int breakableBlocks;  //serialized for debug purposes
+    
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+}
