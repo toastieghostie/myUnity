25e1cbf9d520505f9de473b62b291d8e89c62cc9
unknown <evie@davisplanet.com>
2020-07-20 00:18:29 -0400

    stars n all that :D

---
+++ Glitch Garden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+
+
+    [SerializeField] int stars = 100;
+    Text starText;
+
+    void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    
+}
