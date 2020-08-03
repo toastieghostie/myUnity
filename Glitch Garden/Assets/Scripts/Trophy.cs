87c054046bf945210dfb27afc8b1697155ade950
unknown <evie@davisplanet.com>
2020-08-02 21:58:14 -0400

    fixed cactus again

---
+++ Glitch Garden/Assets/Scripts/Trophy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trophy : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D otherCollider)
+    {
+        Attacker attacker = otherCollider.GetComponent<Attacker>();
+        
+        
+    }
+}
