636171a46886c059b6ae9f9f0caa02704d27b96a
unknown <evie@davisplanet.com>
2020-08-26 22:24:02 -0400

    dust boy added :D

--- Glitch Garden/Assets/Scripts/LevelController.cs
+++ Glitch Garden/Assets/Scripts/LevelController.cs
@@ -27,10 +27,10 @@ public class LevelController : MonoBehaviour
 
     public void AttackerKilled()
     {
-        var livesDisplasy = FindObjectOfType<LivesDisplay>();
+        var livesDisplay = FindObjectOfType<LivesDisplay>();
         
         numberOfAttackers--;
-        if (numberOfAttackers <= 0 && levelTimerFinished && livesDisplasy.lives > 0)
+        if (numberOfAttackers <= 0 && levelTimerFinished && livesDisplay.lives > 0)
         {
             StartCoroutine(HandleWinCondition());
         }
